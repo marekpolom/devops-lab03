4255533060855bcd7310b6ee5629f12d054e0bc8
Marek Połom <marekpolom22@gmail.com>
2021-03-15 14:53:58 +0100

    lab 03

--- band.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS band (
-   id serial PRIMARY KEY,
-   name VARCHAR UNIQUE NOT NULL,
-   creationDate DATE not NULL
-);
-
-insert into band (name, creationdate) values ('Pink Floyd', '01/09/1964')
-insert into band (name, creationdate) values ('Genesis', '01/06/1967')
-insert into band (name, creationdate) values ('Portishead', '01/10/1991')
--- index.js
+++ index.js
@@ -1,102 +1,34 @@
-// Do zdefiniowania aplikacji użyjemy Express.js
 const express = require('express');
 const app = express();
 
-// Nasza aplikacja będzie „konsumowała” dane w formacie JSON
+const port = 3000;
+
 app.use(express.json());
 
-//==================================================================
-// Definiujemy REST API – kod poniżej wymaga uzupełnienia
-//==================================================================
-
-// Pobieranie danych na temat wszystkich zespołów
-app.get('/bands', async (req, res) => {
-  let query = await client.query('SELECT * FROM band');
-
-  return res.send(query.rows);
-});
-
-// Dodawanie rekordów do bazy
-app.post('/bands', async (req, res) => {
-  const message = req.body;
-
-  const query = {
-    text: `INSERT INTO band(${Object.keys(req.body)}) VALUES($1, $2)`,
-    values: Object.values(req.body),
-  }
-
-  await client
-  .query(query)
-  .then(res.send(message))
-  .catch(e => res.send(e.stack))
-
-  // return res.send(message);
-});
-
-// Pobieranie danych na temat zespołu o danej nazwie
-app.get('/bands/:bandName', async (req, res) => {
-  let query = await client.query(`SELECT * FROM band WHERE name='${req.params.bandName}'`);
-  
-  return res.send(query.rows);
-});
-
-// Usuwanie rekordu związanego z zespołem
-app.delete('/bands/:id', async (req, res) => {
-  await client.query(`DELETE FROM band WHERE id=${req.params.id}`);
-
-  return res.send();
-});
-
-// Aktualizacja rekordu związanego z zespołem
-app.put('/bands/:id', async (req, res) => {
-  let id = req.params.id;
-  let keys = Object.keys(req.body);
-
-  let query = ['UPDATE band'];
-  query.push('SET')
-
-  let set = [];
-
-  keys.forEach((key) => {
-    set.push(`${key}='${req.body[key]}'`);
+app.get('/', async (req, res) => {
+  return res.send({
+    get: "get"
   });
-
-  query.push(set.join(', '));
-
-  query.push(`WHERE id=${id}`);
-
-  await client.query(query.join(' '));
-
-  return res.send();
 });
 
-//==================================================================
-// Poniższy kod nie powinien już wymagać zmian
-//==================================================================
+app.post('/', async (req, res) => {
+  return res.send({
+    post: "post"
+  });
+});
 
-// Przygotowujemy/wczytujemy konfigurację połączenia z PostgreSQL-em
-require('dotenv').config();
-const dbConnData = {
-    host: process.env.PGHOST || '127.0.0.1',
-    port: process.env.PGPORT || 5432,
-    database: process.env.PGDATABASE || 'postgres',
-    user: process.env.PGUSER || 'postgres',
-    password: process.env.PGPASSWORD
-};
-// Łączymy się z bazą i „stawiamy” serwer API
-// Do kontaktu z serwerem PostgreSQL wykorzystamy bibliotekę pg
+app.delete('/', async (req, res) => {
+  return res.send({
+    delete: "delete"
+  });
+});
 
-const { Client } = require('pg');
-const client = new Client(dbConnData);
-console.log('Connection parameters: ');
-console.log(dbConnData);
-client
-  .connect()
-  .then(() => {
-    console.log('Connected to PostgreSQL');
-    const port = process.env.PORT || 5000
-    app.listen(port, () => {
-      console.log(`API server listening at http://localhost:${port}`);
-    });
-  })
-  .catch(err => console.error('Connection error', err.stack));
+app.put('/', async (req, res) => {
+  return res.send({
+    put: "put"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`API server listening at http://localhost:${port}`);
+});
